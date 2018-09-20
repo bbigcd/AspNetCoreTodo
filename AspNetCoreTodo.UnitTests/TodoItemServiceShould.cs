adf7bdae46793212e6d75915554afb480d728ea8
陈弟CD <bbigcd@163.com>
2018-09-20 10:33:51 +0800

    create UnitTests

---
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;//首次添加需要重新打开项目
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+    .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+        }
+    }
+}
